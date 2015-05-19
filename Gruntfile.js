b673884b496d7c0ac0e20d758a0d3870bb9a8914
Dan Langston <dan@danlangston.com>
2015-05-19 14:16:42 -0400

    redesign-rb_added banner to plotting and streams, changed path to banner logo img from ../img to /img seems to work so far

--- Gruntfile.js
+++ Gruntfile.js
@@ -932,6 +932,7 @@ module.exports = function(grunt) {
             // Views
             'ooiui/static/js/views/common/DropdownMessagesView.js',
             'ooiui/static/js/views/common/DropdownUserView.js',
+            "ooiui/static/js/views/common/BannerView.js",                                                        
             "ooiui/static/js/views/common/NavbarView.js",
             'ooiui/static/js/views/common/LoginView.js',
             'ooiui/static/js/views/common/ModalDialogView.js',
@@ -977,6 +978,7 @@ module.exports = function(grunt) {
             // Views
             'ooiui/static/js/views/common/DropdownMessagesView.js',
             'ooiui/static/js/views/common/DropdownUserView.js',
+            "ooiui/static/js/views/common/BannerView.js",                                          
             "ooiui/static/js/views/common/NavbarView.js",
             'ooiui/static/js/views/common/LoginView.js',
             'ooiui/static/js/views/common/ModalDialogView.js',
@@ -1187,6 +1189,7 @@ module.exports = function(grunt) {
             "ooiui/static/css/common/newEvent.css",
             "ooiui/static/css/common/ModalForm.css",
             "ooiui/static/css/science/plotting.css",
+            "ooiui/static/css/custom-bs/custom.css"  
           ],
           "ooiui/static/css/compiled/FAQ.css" : [
             "ooiui/static/lib/leaflet/dist/leaflet.css",
