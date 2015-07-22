1636078806fe19039251e0aa4263786bf635a513
Dan Langston <dan@danlangston.com>
2015-07-22 14:21:45 -0400

    third merge coniflict fix

--- ooiui/core/routes/common.py
+++ ooiui/core/routes/common.py
@@ -46,6 +46,10 @@ def user_login():
 def ooi_landing():
    return render_template('ooiLanding/index.html')
 
+@app.route('/science')
+def science():
+   return render_template('science/index.html')
+   
 @app.route('/pioneer-array')
 def pioneer_array():
     return render_template('landing/pioneerArray.html')
